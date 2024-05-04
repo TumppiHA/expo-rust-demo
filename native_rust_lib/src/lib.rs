2daa1307ff71cd8cf3dbd0b013077aad7fabae1f
Tuomas Aarnio <tuomas@aarnio.biz>
2024-05-04 21:46:01 +0300

    cleaner implementation

--- native_rust_lib/src/lib.rs
+++ native_rust_lib/src/lib.rs
@@ -2,22 +2,67 @@ use std::ffi::{CString, CStr};
 use std::os::raw::c_char;
 use image::io::Reader as ImageReader;
 use rqrr;
+use serde::{Serialize, Deserialize};
 
+#[derive(Serialize, Deserialize)]
+struct Bounds {
+    x_max: i32,
+    y_max: i32,
+    x_min: i32,
+    y_min: i32
+}
 
-// Function to create a QR struct by reading an image file
-#[no_mangle]
-pub extern "C" fn read_qr(c_path: *const c_char) -> *const c_char {
-    let path = unsafe {CStr::from_ptr(c_path).to_str().unwrap()};
-    let img = ImageReader::open(path).unwrap().decode().unwrap().to_luma8();
+#[derive(Serialize, Deserialize)]
+struct QRCode {
+    pub content: String,
+    pub bounds: Bounds
+}
+fn _read_qr(path: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let img = ImageReader::open(path.replace("file:", "").as_str())?.decode()?.to_luma8();
     let mut prepared_image = rqrr::PreparedImage::prepare(img);
 
     let grids = prepared_image.detect_grids();
-    assert!(!grids.is_empty(), "No QR codes found");
 
-    let grid = &grids[0];
-    let (_, content) = grid.decode().unwrap();
+    println!("grids: {}", grids.len());
 
-    CString::new(content).unwrap().into_raw()
+    let codes_all: Vec<Result<QRCode, Box<dyn std::error::Error>>> = grids.iter().map(|grid| {
+        let content = match grid.decode() {
+            Ok((_, content)) => content,
+            Err(e) => e.to_string(),
+        };
+
+        let xs = grid.bounds.map(|point| point.x);
+        let ys = grid.bounds.map(|point| point.y);
+
+        let x_max = *xs.iter().max().ok_or("No maximum x found")?;
+        let y_max = *ys.iter().max().ok_or("No maximum y found")?;
+        let x_min = *xs.iter().min().ok_or("No minimum x found")?;
+        let y_min = *ys.iter().min().ok_or("No minimum y found")?;
+
+        let bounds = Bounds {
+            x_max,
+            y_max,
+            x_min,
+            y_min,
+        };
+
+        let code = QRCode { content, bounds };
+        Ok::<QRCode, Box<dyn std::error::Error>>(code)
+    }).collect();
+
+   let codes: Vec<QRCode> = codes_all.into_iter()
+        .filter_map(Result::ok)  // This extracts only Ok values, ignoring Err
+        .collect();
+
+    let s = serde_json::to_string(&codes)?;
+    Ok(s)
+}
+
+#[no_mangle]
+pub extern "C" fn read_qr(c_path: *const c_char) -> *const c_char {
+    let path = unsafe {CStr::from_ptr(c_path).to_str().unwrap()};
+    let res: String = _read_qr(path).unwrap_or_else(|e| e.to_string());
+    unsafe {CString::new(res).unwrap().into_raw()}
 }
 
 /// cbindgen:ignore
@@ -34,12 +79,8 @@ pub mod android {
         class: JClass,
         j_path: JString,
     ) -> jstring {
-
-        // Call the Rust function
-        let content_c_str = read_qr(env.get_string(&j_path).unwrap().into_raw());
-
-        // Convert C string back to Java string
-        let content = unsafe { CStr::from_ptr(content_c_str).to_str().unwrap() };
-        **env.new_string(content).unwrap()
+        let result_c_str = read_qr(env.get_string(&j_path).unwrap().into_raw());
+        let result = unsafe { CStr::from_ptr(result_c_str).to_str().unwrap() };
+        **env.new_string(result).unwrap()
     }
 }
--- native_rust_lib/src/main.rs
+++ native_rust_lib/src/main.rs
@@ -4,7 +4,7 @@ use native_rust_lib::read_qr;
 fn main() {
 
     // Call the read_qr function and directly assume the returned pointer is valid
-    let content_c_str = unsafe { read_qr(CString::new("qr_image.jpg").unwrap().as_ptr()) };
+    let content_c_str = unsafe { read_qr(CString::new("QR-screenshot.png").unwrap().as_ptr()) };
 
     // Convert the C string returned by read_qr back to a Rust String
     println!("Decoded QR Content: {}", unsafe { CStr::from_ptr(content_c_str).to_string_lossy().into_owned() });
