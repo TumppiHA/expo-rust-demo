19184b0e51d46e3378a6d0cb047ccabba77937dc
Tuomas Aarnio <tuomas@aarnio.biz>
2024-05-04 14:33:32 +0300

    scaffolding

--- native_rust_lib/src/main.rs
+++ native_rust_lib/src/main.rs
@@ -1,6 +1,8 @@
+use std::ffi::CString;
 use native_rust_lib::read_qr;
 
 fn main() {
-    let result = read_qr("qr_image.jpg");
-    println!("url: {}", result);
+    let result = read_qr(CString::new("qr_image.jpg").unwrap().as_ptr());
+    unsafe { println!("url: {}", CString::from_raw((*result).content).to_str().unwrap()); }
+    unsafe { println!("bounds: {:?}", (*result).bounds) }
 }
